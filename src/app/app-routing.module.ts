77a33e4c139e7afc584563c4290b8b6b2ec0e8a1
HONG VIP <lexuanhong913@gmail.com>
2023-01-16 11:12:32 +0700

    Merged missing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'xmas_app',
+    loadChildren: () => import('./xmas-app/xmas.module').then(m => m.XmasModule)
+  },
+  {
+    path:'',
+    redirectTo:'xmas_app',
+    pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
